b3896d37a6c217bf22fff54e51421c4c9ddeebae
Steve <stevenjchang@gmail.com>
2017-01-24 23:24:22 +0800

    g controller artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'songs#index'
 end
