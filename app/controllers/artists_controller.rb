086e52a33432652432c2e803070c4a72517ce7f6
Steve <stevenjchang@gmail.com>
2017-01-25 18:24:51 +0800

    Add artists_controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,11 @@
 class ArtistsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+ def index
+   if !params[:artist_name].empty?
+     @artists = RSpotify::Artist.search(params[:artist_name])
+   else
+     redirect_to root_path
+   end
+ end
+ def show
+ end
 end
