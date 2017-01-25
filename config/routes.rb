b322643b8b44338a36174e3b8fed854c13750933
Steve <stevenjchang@gmail.com>
2017-01-25 18:06:22 +0800

    routes configered

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'artists/index'
 
-  get 'artists/show'
+  get '/artists', to: 'artists#index'
+  get '/artists/:id', to:'artists#show', as: 'artist'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'songs#index'
