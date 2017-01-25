19ee9ec46fb29d060fb8eb4363435f42109a814a
Steve <stevenjchang@gmail.com>
2017-01-25 18:08:57 +0800

    Add home_controller view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/artists/:id', to:'artists#show', as: 'artist'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'songs#index'
+  root 'home#index'
 end
